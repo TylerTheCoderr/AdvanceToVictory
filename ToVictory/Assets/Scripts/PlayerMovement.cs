537621307089937f8d51e4683e7c58bdd339aced
TylerTheCoderr <94150600+TylerTheCoderr@users.noreply.github.com>
2021-11-12 23:27:44 +1300

    Update PlayerMovement.cs

--- ToVictory/Assets/Scripts/PlayerMovement.cs
+++ ToVictory/Assets/Scripts/PlayerMovement.cs
@@ -13,5 +13,8 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
