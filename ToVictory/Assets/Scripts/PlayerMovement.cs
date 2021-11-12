cb1da5cd3f021ac24f5ca32562adcb2553ec519e
TylerTheCoderr <94150600+TylerTheCoderr@users.noreply.github.com>
2021-11-13 12:20:11 +1300

    Update PlayerMovement.cs

--- ToVictory/Assets/Scripts/PlayerMovement.cs
+++ ToVictory/Assets/Scripts/PlayerMovement.cs
@@ -12,8 +12,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        float horizontalInput = horizontalInput.GetAxis("Horizontal");
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+        
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
     }
